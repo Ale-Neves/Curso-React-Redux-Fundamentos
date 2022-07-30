58eaa9bd8ac28ddfb1ab57b3d5d865d4604dc925
Alexandre Neves <alexandreneves98@outlook.com>
2022-07-30 13:15:04 -0300

    Trabalhando com Componente Card.

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -2,14 +2,26 @@ import React from 'react';
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
 import Aleatorio from './components/basicos/Aleatorio';
+import Card from './components/layout/Card';
+import './App.css';
 
 export default () => {
   return(
-    <div id='app'>
+    <div className='App'>
       <h1>Fundamentos React</h1>
-      <Aleatorio min={1} max={10}/>
-      <Primeiro />
-      <ComParametro time="Corinthians" nacionalidade="Brasil" estado="SP" />
+      <div className='Cards'>
+        <Card titulo="#03 - Desafio Aleatório" color='#008'>
+          <Aleatorio min={1} max={10}/> 
+        </Card>
+
+        <Card titulo="#02 - Componente com Parametro" color='#080'>
+          <ComParametro time="Corinthians" nacionalidade="Brasil" estado="SP" />
+        </Card>
+
+        <Card titulo="#01 - Primeiro Componentes">
+          <Primeiro />
+        </Card>
+      </div>
     </div>
   );
 }
